df00dc75e0a3334b0d760e1bdf67bc1b55df6018
Newtonkamau14 <newtonkamau14@gmail.com>
2022-07-22 19:32:31 +0300

    adding review page

--- routes/reviewsController.js
+++ routes/reviewsController.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const Review = require('./../models/review.model')
 const router = express.Router()
 
+//Get review page
 router.get('/', (req,res) => {
     res.render('pages/reviews',{
         title: "Reviews"
     });
 });
+
+//Post review 
+router.post('/',async(req,res) => {
+    let review  = new Review({
+        restaurantName: req.body.restaurantName,
+        mealType: req.body.mealType,
+        stars: req.body.stars,
+        description: req.body.description
+    });
+    try {
+        review = await review.save();
+        res.redirect('/')
+        
+    } catch (error) {
+        console.log(error);
+        res.render('/review')        
+    }
+})
 
 
 module.exports = router;
